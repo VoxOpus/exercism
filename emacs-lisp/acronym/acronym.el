d394c9f53e88ebdab9ba5744a5c74cf61583b479
VoxOpus <nathanieldenisludtke@gmail.com>
2026-09-06 02:21:41 +0200

    chore(emacs-list): start acronym

---
+++ emacs-lisp/acronym/acronym.el
@@ -0,0 +1,12 @@
+;;; acronym.el --- Acronym (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defun acronym (phrase)
+ (error "Delete this S-Expression and write your own implementation"))
+
+(provide 'acronym)
+;;; acronym.el ends here
